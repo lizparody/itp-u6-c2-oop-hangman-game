c48b29e91dbe89826cae7fe88282337db8ae1942
Liz Parody <lizparody@gmailo.com>
2017-08-22 17:34:04 +0000

    Added the GuessAttempt and the GuessWord function

--- hangman/game.py
+++ hangman/game.py
@@ -2,12 +2,35 @@ from .exceptions import *
 
 
 class GuessAttempt(object):
-    pass
-
-
+    def __init__(self, character, hit=False, miss=False):
+        self.character = character
+        self.hit = hit
+        self.miss = miss
+        if self.hit == True and self.miss == True:
+            raise InvalidGuessAttempt
+    
+    def is_hit(self):
+        return self.hit
+    
+    def is_miss(self):
+        return self.miss
+        
+  
 class GuessWord(object):
-    pass
+    def __init__(self, answer):
+        self.answer = answer
+        if self.answer == '':
+            raise InvalidWordException
+        self.masked = len(answer) * '*'
+        
+    def perform_attempt(self, character):
+        if len(character) > 1:
+            raise InvalidGuessedLetterException
+        if character in self.answer:
+            return GuessAttempt(character, hit=True)
+        else:
+            return GuessAttempt(character, miss=True)
 
-
+            
 class HangmanGame(object):
     pass
